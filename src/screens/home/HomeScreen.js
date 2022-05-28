af080c7e879150b7868e27b7dc15d6b7523dcc41
Mohammad Reza Salemi <en.salemi75@gmail.com>
2022-05-28 10:19:38 +0430

    Remove background color from home screen

--- src/screens/home/HomeScreen.js
+++ src/screens/home/HomeScreen.js
@@ -1,4 +1,4 @@
-import {SafeAreaView, View, FlatList, StyleSheet} from 'react-native';
+import {SafeAreaView, FlatList, StyleSheet} from 'react-native';
 import React, {useState, useEffect, useCallback} from 'react';
 
 import {Card} from '../../components/Card';
@@ -35,49 +35,21 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView style={styles.flexGrow}>
-      <View style={styles.flexGrow}>
-        <View style={styles.cardZIndx}>
-          <FlatList
-            data={users}
-            renderItem={renderItem}
-            keyExtractor={keyExtractor}
-            onEndReachedThreshold={0.1}
-            onEndReached={loadMore}
-            ListHeaderComponent={HomeHeader}
-            ListFooterComponent={RenderLoader}
-          />
-        </View>
-
-        <View style={styles.userBack}>
-          <View style={styles.userTopBack} />
-          <View style={styles.userBottomBack} />
-        </View>
-      </View>
+      <FlatList
+        data={users}
+        renderItem={renderItem}
+        keyExtractor={keyExtractor}
+        onEndReachedThreshold={0.1}
+        onEndReached={loadMore}
+        ListHeaderComponent={HomeHeader}
+        ListFooterComponent={RenderLoader}
+      />
     </SafeAreaView>
   );
 };
 const styles = StyleSheet.create({
   flexGrow: {
     flex: 1,
-  },
-  cardZIndx: {
-    zIndex: 0,
-  },
-  userBack: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    zIndex: -1,
-  },
-  userTopBack: {
-    height: 110,
-    backgroundColor: '#001F2D',
-  },
-  userBottomBack: {
-    flex: 1,
-    backgroundColor: 'white',
   },
 });
 export default HomeScreen;
